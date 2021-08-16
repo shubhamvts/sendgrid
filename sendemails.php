3ed6c5947361c8ca825630310739e38da520e69e
shubhamvts <71962042+shubhamvts@users.noreply.github.com>
2021-08-17 00:28:16 +0530

    Update sendemails.php

--- sendemails.php
+++ sendemails.php
@@ -11,7 +11,7 @@ class SendEmail{
     $email->addContent("text/html",$content);
      $url="https://imgs.xkcd.com/s/a899e84.jpg";
     
-$att1 = new \SendGrid\Attachment();
+$att1 = new \SendGrid\Mail\Attachment();
 $att1->setContent(base64_encode(file_get_contents($url)));
 $att1->setType("image/jpeg");
 $att1->setFilename("photo.");
